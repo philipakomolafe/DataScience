836f28af7fd9752fd034516744260176f86ae676
philipakomolafe <akomolafephilip@gmail.com>
2024-02-03 18:06:03 +0100

    Day_2

--- lesson11/build.py
+++ lesson11/build.py
@@ -1,38 +1,20 @@
-# lets build fun stuff According to the neato's of python.
-# lets say we wanna build a game that walk you through an architectural layout 😅😁
-# lets get coding!!!!!
-
-# from random import choice, randint
-
-# # here python represents your tour-guide.
-# python = choice("123456")
-
-# # And of-course the user is the player 😁..
-# user = input("Select Either of Any..\n1- Door in Front\n2- Open Door\n3- Move Forward\n4- Turn Left\n5- Climb Stairs\n6- Move Barkward\n\n")
-
-# # NOTE POSSIBILITY of USER entering WRONG INPUTS
-# usr_chose = int(user)
-# print(f"USER CHOSE: {usr_chose}")
-
-# def if_else(integer):
-
-#     if (integer % 2 != 0):
-#         return "Weird"
-#     elif (integer in range(2, 6)) and (integer % 2 == 0):
-#         return "Not Weird"
-#     elif (integer in range(6, 21)) and (integer % 2 == 0):
-#         return "Weird"
-#     elif (integer % 2 == 0) and (integer > 20):
-#         return "Not Weird"
+# Day Two😁😂😂😁😁
 
 
-# if __name__ == "__main__":
-#     n = int(input().strip())
+def solve(meal_cost, tip_percent, tax_percent):
+    """
+    this helps in fetching the cost of items(meals e.t.c), percentage tips (on meals) & tax (on meals)
+    """
+    tip = (meal_cost * 0.01) * tip_percent
+    tax = (tax_percent * 0.01) * meal_cost
 
-#     result = if_else(n)
-#     print(f"Pick 'em ALL: {result}")
+    total_cost = meal_cost + tip + tax
+    print(round(total_cost))
+
 
 if __name__ == "__main__":
-    n = int(input("Enter Number within 1-100.."))
-    if n in []:
-        pass
+    meal_cost = float(input().strip())
+    tip_percent = int(input().strip())
+    tax_percent = int(input().strip())
+
+    solve(meal_cost, tip_percent, tax_percent)
